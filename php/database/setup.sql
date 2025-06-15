e1fa23bd3269ec441ce8e2ef100c9502dc0071e6
JorgeCsstro <jorgecastrot2005@gmail.com>
2025-06-15 23:05:57 +0200

    Habitus: DONE

--- php/database/setup.sql
+++ php/database/setup.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 15-06-2025 a las 21:04:28
+-- Tiempo de generación: 15-06-2025 a las 21:05:27
 -- Versión del servidor: 10.11.10-MariaDB
 -- Versión de PHP: 7.2.34
 
